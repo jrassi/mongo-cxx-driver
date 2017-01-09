35ab94b2a78ea052bfc73e9534d601d70720a787
J. Rassi <rassi@10gen.com>
2017-01-09 08:07:17 -0800

    more_progress

--- .evergreen/install.sh
+++ .evergreen/install.sh
@@ -15,12 +15,31 @@ usage() {
 }
 [ $# -lt 1 ] && { usage; exit 2; }
 
-CONFIGURE_ARGS_MONGOC="--disable-automatic-init-and-cleanup --disable-shm-counters --with-libbson=system"
-CONFIGURE_ARGS_BSON="--disable-extra-align"
-CONFIGURE_ARGS_EXTRA="--enable-tests=no --enable-examples=no --enable-debug --enable-optimizations --disable-static --disable-dependency-tracking --with-pic"
 VERSION=${2:-master}
 PREFIX=${PREFIX:-$(pwd)"/deps-install"}
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+UNIX_CONFIGURE_ARGS_MONGOC="--disable-automatic-init-and-cleanup --disable-shm-counters --with-libbson=system"
+UNIX_CONFIGURE_ARGS_BSON="--disable-extra-align"
+UNIX_CONFIGURE_ARGS_EXTRA="--enable-tests=no --enable-examples=no --enable-debug --enable-optimizations --disable-static --disable-dependency-tracking --with-pic --prefix=\"$PREFIX\""
+
+WINDOWS_CONFIGURE_ARGS_MONGOC=
+WINDOWS_CONFIGURE_ARGS_BSON=
+WINDOWS_CONFIGURE_ARGS_EXTRA="-DCMAKE_INSTALL_PREFIX=\"$PREFIX\""
+
+case "$OS" in
+	darwin|linux)
+        CONFIGURE_ARGS_MONGOC="$UNIX_CONFIGURE_ARGS_MONGOC"
+        CONFIGURE_ARGS_BSON="$UNIX_CONFIGURE_ARGS_BSON"
+        CONFIGURE_ARGS_EXTRA="$UNIX_CONFIGURE_ARGS_EXTRA"
+		;;
+
+    cygwin*)
+        CONFIGURE_ARGS_MONGOC="$WINDOWS_CONFIGURE_ARGS_MONGOC"
+        CONFIGURE_ARGS_BSON="$WINDOWS_CONFIGURE_ARGS_BSON"
+        CONFIGURE_ARGS_EXTRA="$WINDOWS_CONFIGURE_ARGS_EXTRA"
+        ;;
+esac
 
 case "$1" in
 	libbson)
@@ -73,8 +92,7 @@ case "$OS" in
 		;;
 
 	cygwin*)
-		/cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX="$PREFIX"
-		# TODO: translate and use configure args.
+		/cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" $CONFIGURE_ARGS
 		"/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" /m INSTALL.vcxproj
 		;;
 esac
